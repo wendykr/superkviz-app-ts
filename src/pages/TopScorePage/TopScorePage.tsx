3a972983f72dee84ea5b8a3b8b28b4d8523cb43c
Vendula Krajíčková <vendysacek@seznam.cz>
2024-05-28 10:58:19 +0200

    add loading for top score

--- src/pages/TopScorePage/TopScorePage.tsx
+++ src/pages/TopScorePage/TopScorePage.tsx
@@ -10,27 +10,34 @@ export interface TopScoreStructure {
 
 export const TopScorePage: React.FC = () => {
   const [topScoreData, setTopScoreData] = useState<TopScoreStructure[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     getTopScore();
   }, []);
 
   const getTopScore = async (): Promise<void> => {
+    setIsLoading(true);
+    setError(null);
+
     try {
       const { data: topscore, error } = await supabase
         .from('topscore')
         .select('*');
 
       if (error) {
-        console.error('Chyba při načítání dat:', error);
+        setError('Chyba při načítání dat: ' + error.message);
+        setIsLoading(false);
         return;
       }
 
       if (topscore) {
         setTopScoreData(topscore);
+        setIsLoading(false);
       }
     } catch (error) {
-      console.error('Neočekávaná chyba při načítání dat:', error);
+      setError('Neočekávaná chyba při načítání dat: ' + (error as Error).message);
     }
   };
 
@@ -38,9 +45,15 @@ export const TopScorePage: React.FC = () => {
     <div className="topscore">
 
       <h2 className="topscore__title">Žebříček nejlepších</h2>
+      {isLoading && <p>Načítání dat...</p>}
+      {error && !isLoading && <p>{error}</p>}
 
-      <TopScoreList topScoreData={topScoreData} />
-
+      {
+        topScoreData && topScoreData.length > 0 ?
+          <TopScoreList topScoreData={topScoreData} />
+          :
+          <p className="topscore__content">... je prázdný!</p>
+      }
   </div>
   )
 }
