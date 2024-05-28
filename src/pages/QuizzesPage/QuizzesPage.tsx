db7281cfd73ae3dd05eaa7b13b42b34d4e0fb105
Vendula Krajíčková <vendysacek@seznam.cz>
2024-05-28 11:15:50 +0200

    add loading for quiz list

--- src/pages/QuizzesPage/QuizzesPage.tsx
+++ src/pages/QuizzesPage/QuizzesPage.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import './QuizzesPage.css';
 import { QuizList } from "../../components/QuizList/QuizList";
 import { supabase } from '../../supabaseClient';
 
@@ -15,34 +16,51 @@ interface QuizzesPageProps {
 
 export const QuizzesPage: React.FC<QuizzesPageProps> = ({ onHandleReset }) => {
   const [quizzesData, setQuizzesData] = useState<QuizzesStructure[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     getQuizzes();
   }, []);
 
   const getQuizzes = async (): Promise<void> => {
+    setIsLoading(true);
+    setError(null);
+
     try {
       const { data: quizzes, error } = await supabase
         .from('quizzes')
         .select('*');
 
       if (error) {
-        console.error('Chyba při načítání dat:', error);
+        setError('Chyba při načítání dat: ' + error.message);
+        setIsLoading(false);
         return;
       }
 
       if (quizzes) {
         setQuizzesData(quizzes);
+        setIsLoading(false);
       }
     } catch (error) {
-      console.error('Neočekávaná chyba při načítání dat:', error);
+      setError('Neočekávaná chyba při načítání dat: ' + (error as Error).message);
     }
   };
 
   return (
-    <QuizList
-      quizzesData={quizzesData}
-      onHandleReset={onHandleReset}
-    />
+    <>
+      {isLoading ? (
+        <p className="quizzes__content">Načítání dat...</p>
+      ) : (
+        <>
+          {error && <p className="quizzes__content">{error}</p>}
+          {quizzesData && quizzesData.length > 0 ? (
+            <QuizList quizzesData={quizzesData} onHandleReset={onHandleReset} />
+          ) : (
+            <p className="quizzes__content">Databáze neobsahuje žádné kvízy!</p>
+          )}
+        </>
+      )}
+    </>
   );
 };
