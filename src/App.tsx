9dfb6db3868747ca74796c314421ec913ccc8865
Vendula Krajíčková <vendysacek@seznam.cz>
2024-05-28 11:27:30 +0200

    add justify start for top score

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css'
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
 
 function App() {
+  const location = useLocation();
+  const isTopScorePage = location.pathname.includes("topscore");
 
   return (
     <>
       <Header />
-      <main className="main">
+      <main className={`main ${isTopScorePage ? 'topscore' : ''}`}>
         <Outlet />
       </main>
       <Footer />
